7ec717aee733833277c4ee8bdf70c81a9d2ad6ab
Ian Walter <public@iankwalter.com>
2019-04-29 11:39:45 -0400

    Working on upgrading to corejs 3

--- tests.js
+++ tests.js
@@ -1,43 +1,43 @@
-import test from 'ava'
-import merge from '.'
+const { test } = require('@ianwalter/bff')
+const merge = require('.')
 
-test('shallow Objects get merged', t => {
+test('shallow Objects get merged', ({ expect }) => {
   const obj1 = { count: 1, color: 'green' }
   const obj2 = { count: 1, shape: 'triangle' }
   const obj3 = { count: 1, size: 'large' }
-  t.deepEqual(merge(obj1, obj2, obj3), { ...obj1, ...obj2, ...obj3 })
+  expect(merge(obj1, obj2, obj3)).toEqual({ ...obj1, ...obj2, ...obj3 })
 })
 
-test('nested Objects get merged', t => {
+test('nested Objects get merged', ({ expect }) => {
   const general = { screwdriver: { type: 'Phillips-Head' } }
   const auto = { safety: ['Welding Gloves'] }
   const obj1 = { id: 'a', tools: { auto: ['Lift'] } }
   const obj2 = { id: 'b', tools: { auto: ['Pressure Gauge'], general } }
   const obj3 = { id: 'c', tools: { auto } }
-  t.deepEqual(merge(obj1, obj2, obj3), { id: 'c', tools: { auto, general } })
+  expect(merge(obj1, obj2, obj3)).toEqual({ id: 'c', tools: { auto, general } })
 })
 
-test('nested Arrays get replaced', t => {
+test('nested Arrays get replaced', ({ expect }) => {
   const obj1 = { count: 1, items: [1] }
   const obj2 = { count: 1, items: [2] }
   const obj3 = { count: 1, items: [3] }
-  t.deepEqual(merge(obj1, obj2, obj3), obj3)
+  expect(merge(obj1, obj2, obj3)).toEqual(obj3)
 })
 
-test('null values are not treated as objects', t => {
+test('null values are not treated as objects', ({ expect }) => {
   const obj1 = { id: 'a', tools: { auto: { safety: ['Welding Gloves'] } } }
   const obj2 = { id: 'b', tools: { auto: null } }
-  t.deepEqual(merge(obj1, obj2), obj2)
+  expect(merge(obj1, obj2)).toEqual(obj2)
 })
 
-test('prototype properties get merged', t => {
+test('prototype properties get merged', ({ expect }) => {
   const obj1 = { run: () => 'RUN', log: () => 'LOG' }
   Object.setPrototypeOf(obj1, { dog: () => 'DOG' })
   const obj2 = { run: v => `RUN: ${v}` }
   Object.setPrototypeOf(obj2, { fog: () => 'FOG' })
   const obj3 = merge({}, obj1, obj2)
-  t.is(obj3.run('now'), 'RUN: now')
-  t.is(obj3.log(), 'LOG')
-  t.is(obj3.dog(), 'DOG')
-  t.is(obj3.fog(), 'FOG')
+  expect(obj3.run('now')).toBe('RUN: now')
+  expect(obj3.log()).toBe('LOG')
+  expect(obj3.dog()).toBe('DOG')
+  expect(obj3.fog()).toBe('FOG')
 })
